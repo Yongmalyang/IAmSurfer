835b8a13aa885ecf31238aa120694ac521e27cb4
yoongang02 <yoongang02@gmail.com>
2021-12-28 02:19:36 +0900

    블루홀 보드 랜덤생성

---
+++ IAmSurfer_2021/IAmSurfer/Assets/Scripts_BlueHole/SurfBoardSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfBoardSpawn : MonoBehaviour
+{
+    void Start()
+    {
+        // 랜덤 위치 설정
+        float spawnY = Random.Range
+                    (26.1f, 27.0f);
+        float spawnX = Random.Range
+            (-7.3f, 7.0f);
+
+        transform.position = new Vector2(spawnX, spawnY);
+    }
+
+    void Update()
+    {
+        
+    }
+}
