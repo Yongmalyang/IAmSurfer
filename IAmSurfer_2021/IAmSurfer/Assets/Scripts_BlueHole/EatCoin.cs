a68985152bf32b6da950ceb1d9c1ea580ffef571
Haeun-Y <hahaha329@ewhain.net>
2021-12-28 06:00:33 +0900

    동전 먹기, 배경음 추가

--- IAmSurfer_2021/IAmSurfer/Assets/Scripts_BlueHole/EatCoin.cs
+++ IAmSurfer_2021/IAmSurfer/Assets/Scripts_BlueHole/EatCoin.cs
@@ -7,11 +7,13 @@ public class EatCoin : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         bool isCoin = collision.gameObject.name.Contains("coin");
+        AudioSource sound_getCoin = GetComponent<AudioSource>();
 
         if (isCoin) //코인 획득 시 돈 증가
         {
             PlayerController.Instance.playerData.money += 1;
             Debug.Log(PlayerController.Instance.playerData.money);
+            sound_getCoin.Play();
         }
             
         collision.gameObject.SetActive(false);
